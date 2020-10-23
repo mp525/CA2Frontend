ab630b1a59dbf6f661ac6091f403914b8aab737c
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-10-23 15:18:28 +0200

    addPerson wip

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-document.getElementById("getByPhone").onclick=getByPhone;
+document.getElementById("getByPhone").onclick = getByPhone;
 
 //Get a Person using a phone number
 function getByPhone() {
     let phone = document.getElementById("phone").value;
-    let url="https://mparking.dk/CA2Backend/api/person/phone/"+phone;
-    const str="";
+    let url = "https://mparking.dk/CA2Backend/api/person/phone/" + phone;
+    const str = "";
     fetch(url)
-    
+
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            
-            document.getElementById("table").innerHTML="<tr><th>"+data.firstName+"</th><th>"+data.lastName+"</th><th>"+data.email+"</th><th>"+data.street+"</th><th>"+data.zip+"</th><th>"+data.houseNr+"</th></tr>";
-            let str="";
-            var mapHobbies=data.hobbies.map(x=>{
-                return "<tr><th>"+x.name+"</th><th>"+x.type+"</th></tr>"
+
+            document.getElementById("table").innerHTML = "<tr><th>" + data.firstName + "</th><th>" + data.lastName + "</th><th>" + data.email + "</th><th>" + data.street + "</th><th>" + data.zip + "</th><th>" + data.houseNr + "</th></tr>";
+            let str = "";
+            var mapHobbies = data.hobbies.map(x => {
+                return "<tr><th>" + x.name + "</th><th>" + x.type + "</th></tr>"
             });
-            document.getElementById("table2").innerHTML=mapHobbies;
+            document.getElementById("table2").innerHTML = mapHobbies;
             console.log(data);
         });
-        
+
 }
 //Method to use endpoint to retrieve all people who participate in a given hobby
 function getAllByHobby() {
     let Hobby = document.getElementById("Hobby").value;
-    let url="https://mparking.dk/CA2Backend/api/person/byhobby/"+Hobby;
+    let url = "https://mparking.dk/CA2Backend/api/person/byhobby/" + Hobby;
     fetch(url)
-    
+
         .then(function (response) {
             return response.json();
         })
@@ -37,33 +37,33 @@ function getAllByHobby() {
 }
 
 
-document.getElementById("submitCount").onclick=getCountHobby;
+document.getElementById("submitCount").onclick = getCountHobby;
 //Get number of people by hobby
 function getCountHobby() {
     const hobbyName = document.getElementById("count").value;
-    const url = "https://mparking.dk/CA2Backend/api/person/countByHobby/"+hobbyName;
+    const url = "https://mparking.dk/CA2Backend/api/person/countByHobby/" + hobbyName;
 
     fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        document.getElementById("table2").innerHTML="<p>Number of people with the hobby "+hobbyName+": "+data.count+"</p>";
-        console.log(data.count);
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            document.getElementById("table2").innerHTML = "<p>Number of people with the hobby " + hobbyName + ": " + data.count + "</p>";
+            console.log(data.count);
+        });
 }
 
 document.getElementById("getAllZips").onclick = getAllZips;
 function getAllZips() {
 
-    
+
     console.log("We have connection");
 
 
     let url = "https://mparking.dk/CA2Backend/api/person/allZips";
-    
+
     fetch(url)
-    
+
         .then(function (response) {
             return response.json();
         })
@@ -73,32 +73,101 @@ function getAllZips() {
             })
             document.getElementById("table").innerHTML = mapped.join("");
         });
-    
-    
-    }
-    const btnZip = document.getElementById("getByZip").addEventListener("click", getByZip);
 
-    function getByZip(e) {
-        let zipcode = document.getElementById("zip").value;
-        let url = "https://mparking.dk/CA2Backend/api/person/allWithZip/" + zipcode;
-        fetch(url)
-            .then(function (res) {
-                return res.json();
-            })
-            .then(function (data) {
-                console.log(data);
-        
-                let people = data.map(function (p) {
-                    let hobbies = p.hobbies.map((h) => { return h.name });
-                    let phones = p.phones.map((ph) => { return ph.number });
-                    return "<tr><td>" + p.firstName + "</td><td>" + p.lastName + "</td><td>" + p.email
-                        + "</td><td>" + phones.join(", ") + "</td><td>" + hobbies.join(", ") + "</td></tr>";
-                });
-                people.unshift("<th>First name</th><th>Last Name</th><th>Email</th><th>Phones</th><th>Hobbies</th>");
-                document.getElementById("table").innerHTML = people.join("");
+
+}
+const btnZip = document.getElementById("getByZip").addEventListener("click", getByZip);
+
+function getByZip() {
+    let zipcode = document.getElementById("zip").value;
+    let url = "https://mparking.dk/CA2Backend/api/person/allWithZip/" + zipcode;
+    fetch(url)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            let people = data.map(function (p) {
+                let hobbies = p.hobbies.map((h) => { return h.name });
+                let phones = p.phones.map((ph) => { return ph.number });
+                return "<tr><td>" + p.firstName + "</td><td>" + p.lastName + "</td><td>" + p.email
+                    + "</td><td>" + phones.join(", ") + "</td><td>" + hobbies.join(", ") + "</td></tr>";
             });
-        document.getElementById("closeZip").click();
+            people.unshift("<th>First name</th><th>Last Name</th><th>Email</th><th>Phones</th><th>Hobbies</th>");
+            document.getElementById("table").innerHTML = people.join("");
+        });
+    document.getElementById("closeZip").click();
 
+}
+
+function hobbyList(e) {
+
+}
+
+const btnAdd = document.getElementById("addPerson").addEventListener("click", addPerson);
+const btnHobby = document.getElementById("hobbiesP").addEventListener("click", chosen);
+
+function chosen(e) {
+    let target = e.target;
+    chosen = target.id;
+    document.getElementById("chosen").innerHTML = target.id;
+}
+
+function addPerson() {
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let email = document.getElementById("email").value;
+    let street = document.getElementById("street").value;
+    let houseNr = document.getElementById("houseNr").value;
+    let zip = document.getElementById("zipRegister").value;
+    let phoneNr = document.getElementById("phoneNr").value;
+    let phoneDisc = document.getElementById("phoneDisc").value;
+    let hobbyName = document.getElementById("chosen").value;
+
+    let url = "https://mparking.dk/CA2Backend/api/person";
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            street,
+            houseNr,
+            zip,
+            phoneNr,
+            phoneDisc,
+            hobbyName
+        })
     }
+    fetch(url, options)
+        .then(res => fetchWithErrorCheck(res))
 
-  
+        .then((data) => {
+            document.getElementById("succesAdd").innerHTML = "Succes!";
+        })
+        .catch(err => {
+            if (err.status) {
+                err.fullError.then(e => console.log(e.detail))
+            }
+            else {
+                console.log("Network error");
+                document.getElementById("succesAdd").innerHTML = "An error has occured!";
+            }
+        });
+
+
+}
+function fetchWithErrorCheck(res) {
+    if (!res.ok) {
+        return Promise.reject({ status: res.status, fullError: res.json() })
+    }
+    return res.json();
+}
+
+
